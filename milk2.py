c6fe0e67f22deb396709ce36a56b5e176aece042
kanghee <kanghee.cho7@gmail.com>
2020-07-19 19:06:21 -0700

    simplifed function

--- milk2.py
+++ milk2.py
@@ -5,45 +5,38 @@ PROG: milk2
 """
 fin = open('milk2.in')
 fout = open('milk2.out','w')
+lines_number = int(fin.readline())
 time_list = []
-for line in fin.readlines():
-    if ' 'in line:
-        time_list.append(tuple(int(x) for x in line.strip().split()))
-
+for line in range(lines_number):
+    line = fin.readline()
+    time_list.append(tuple(int(x) for x in line.strip().split()))
 def FindTimeOverlap(time1tuple, time2tuple):
     time1_start, time1_end = time1tuple
     time2_start, time2_end = time2tuple
     if time1_start <= time2_start <= time1_end or time2_start <= time1_start <= time2_end:
-        return (min(time1_start, time2_start), max(time1_end, time2_end))
+        return True, [(min(time1_start, time2_start), max(time1_end, time2_end))]
     else:
-        return [(time1tuple, time2tuple)]
+        return False, [time2_start-time1_end]
 time_list.sort()
-overlap_list = [time_list[0]]
+overlap_list = []
 idle_list = []
 overlap = time_list[0]
-for item in time_list:
-    current_overlap = FindTimeOverlap(overlap, item)
-    if type(current_overlap) == tuple:
+for item in time_list[1:]:
+    condition, [current_overlap] = FindTimeOverlap(overlap, item)
+    if condition:
         overlap = current_overlap
     else:
         overlap_list.append(overlap)
-        idle_list += current_overlap
+        idle_list.append(current_overlap)
         overlap = item
-if overlap != item:
-    overlap_list.append(overlap)
+overlap_list.append(overlap)
 
 largest_overlap = 0
 for overlaps in overlap_list:
     start, end = overlaps
     if end-start > largest_overlap:
         largest_overlap = end-start
-largest_idle = 0
-for idling in idle_list:
-    tuple1, tuple2= idling
-    start1, end1 = tuple1
-    start2, end2 = tuple2
-    if start2-end1 > largest_idle:
-        largest_idle = start2-end1
+largest_idle = max(idle_list)
 
 fout.write(f"{largest_overlap} {largest_idle}\n")
 
