bc7dbb4d3d77d61258db0117645e7961f01a0d71
kanghee <kanghee.cho7@gmail.com>
2020-07-19 19:25:16 -0700

    simplifed function even more

--- milk2.py
+++ milk2.py
@@ -10,33 +10,27 @@ time_list = []
 for line in range(lines_number):
     line = fin.readline()
     time_list.append(tuple(int(x) for x in line.strip().split()))
-def FindTimeOverlap(time1tuple, time2tuple):
+def IsOverlap(time1tuple, time2tuple):
     time1_start, time1_end = time1tuple
     time2_start, time2_end = time2tuple
     if time1_start <= time2_start <= time1_end or time2_start <= time1_start <= time2_end:
-        return True, [(min(time1_start, time2_start), max(time1_end, time2_end))]
-    else:
-        return False, [time2_start-time1_end]
+        return True
+    return False
 time_list.sort()
-overlap_list = []
-idle_list = []
+largest_overlap = 0
+largest_idle = 0
 overlap = time_list[0]
 for item in time_list[1:]:
-    condition, [current_overlap] = FindTimeOverlap(overlap, item)
-    if condition:
-        overlap = current_overlap
+    item_start, item_end = item
+    overlap_start, overlap_end = overlap
+    if IsOverlap(item, overlap):
+        overlap = min(item_start, overlap_start), max(overlap_end, item_end)
     else:
-        overlap_list.append(overlap)
-        idle_list.append(current_overlap)
+        if overlap_end-overlap_start > largest_overlap:
+            largest_overlap = overlap_end-overlap_start
+        if item_start-overlap_end > largest_idle:
+            largest_idle = item_start-overlap_end
         overlap = item
-overlap_list.append(overlap)
-
-largest_overlap = 0
-for overlaps in overlap_list:
-    start, end = overlaps
-    if end-start > largest_overlap:
-        largest_overlap = end-start
-largest_idle = max(idle_list)
 
 fout.write(f"{largest_overlap} {largest_idle}\n")
 
